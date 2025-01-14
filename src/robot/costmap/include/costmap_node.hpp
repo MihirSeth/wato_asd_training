7ea928b45ae537d6e6abec264a0e2dbeeb31b309
Albear-Yang <albert.yang727@gmail.com>
2025-01-14 14:00:51 -0500

    .

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,16 +1,26 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
-
+ 
 #include "rclcpp/rclcpp.hpp"
-
+#include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include "costmap_core.hpp"
-
+#include "nav_msgs/msg/occupancy_grid.hpp"
+ 
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
-
+    
+    // Place callback function here
+    void publishMessage(float x);
+    void lidar_sub(const sensor_msgs::msg::LaserScan::SharedPtr scan);
+ 
   private:
     robot::CostmapCore costmap_;
+    // Place these constructs here
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
+    //rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr lidar_sub_;
 };
-
+ 
 #endif 
--- src/robot/costmap/src/costmap_node.cpp
+++ src/robot/costmap/src/costmap_node.cpp
@@ -1,9 +1,31 @@
 #include <chrono>
 #include <memory>
+#include <string>
 
 #include "costmap_node.hpp"
+ 
+CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {
+  // Initialize the constructs and their parameters
+  string_pub_ = this->create_publisher<std_msgs::msg::String>("/test_topic", 50);
+  lidar_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    "/lidar", 10, std::bind(&CostmapNode::lidar_sub, this, std::placeholders::_1));
+  //timer_ = this->create_wall_timer(std::chrono::milliseconds(500), std::bind(&CostmapNode::publishMessage, this));
+}
+ 
+// Define the timer to publish a message every 500ms
+void CostmapNode::publishMessage(float x) {
+  auto message = std_msgs::msg::String();
+  message.data = std::to_string(x);
+  RCLCPP_INFO(this->get_logger(), "Coord: '%s'", message.data.c_str());
+  string_pub_->publish(message);
+}
 
-CostmapNode::CostmapNode() : Node("costmap"), costmap_(robot::CostmapCore(this->get_logger())) {}
+void CostmapNode::lidar_sub(const sensor_msgs::msg::LaserScan::SharedPtr scan){
+  auto message = std_msgs::msg::String();
+  for(auto i: scan->ranges){
+   publishMessage(i); 
+  }
+}
 
 int main(int argc, char ** argv)
 {
